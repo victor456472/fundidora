483142457f784ed941e257ef8bb14e014259d1ae
victor456472 <victor456472@gmail.com>
2023-05-17 16:13:44 -0500

    añadiendo variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 
+float dist_pared = 15; //ojo definir en el experimento
+int t1=20; // definir en el experimento
+int t2=20; // definir en el experimento
+int t3=20; // definir en el experimento
+
 void setup() {
   // put your setup code here, to run once:
 }
