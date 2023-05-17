810f4191ca864e3eba764571addbf565b70898e2
victor456472 <victor456472@gmail.com>
2023-05-17 16:48:23 -0500

    añadiendo dependencias del servomotor

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <Servo.h>
+
 int boton;
 bool door1=true;
 bool door2=true;
@@ -9,8 +11,28 @@ int t1=20; // definir en el experimento
 int t2=20; // definir en el experimento
 int t3=20; // definir en el experimento
 
+const int pos_inicial = 20; //definir en el experimento
+const int pos_final = 20; //definir en el experimento
+unsigned long t_current;
+unsigned long t_init;
+
+//servomotor
+
+int pulsemin=650;
+int pulsemax=3600;
+
+int pos_inicial = 0; //definir en el experimento
+int pos_final = 180; //definir en el experimento
+
+Servo servomotor;
+
 void setup() {
-  // put your setup code here, to run once:
+  servomotor.attach(10, pulsemin, pulsemax); //verificar pin disponible para el servo
+  servomotor.write(pos_inicial);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(4, INPUT);
+  t_init = millis();
 }
 
 void loop() {
